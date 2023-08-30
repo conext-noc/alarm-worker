a7608a7ed216c5e61b0e3fb24d58f3216c8ba21a
Ricardo Vera <ricardovera71@gmail.com>
2023-08-29 21:20:09 -0500

    fix(time-starters)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 from dotenv import load_dotenv
 from helpers.utils.ssh import ssh
 from helpers.constants.definitions import olt_devices, endpoints
@@ -10,28 +11,27 @@ load_dotenv()
 
 
 def main():
-    interval = 4 * 60 * 60
     clients = []
     count = 1
     while True:
-        print(f"loop #{count}")
-        for olt in range(1, 2):
-            start_time = time.time()
-            (comm, command, quit_ssh) = ssh(olt_devices[str(olt)])
-            command("scroll 512")
-            clts = client_ports(comm, command, str(olt))
-            clients.extend(clts)
-            quit_ssh()
-            end_time = time.time()
-            ttl_time = end_time - start_time
-            print(
-                f"the ttl amount of time for a given olt [max] is : {ttl_time} secs | {ttl_time/60} min"
-            )
-        db_request(endpoints["empty_alarms"], {})
-        db_request(endpoints["add_alarms"], {"alarms": clients})
-        # send_mail(clients)
-        count += 1
-        time.sleep(interval)
+        if bool(datetime.now().strftime("%I%p") in ['12PM', '04AM', '08AM', '12AM', '04PM', '08PM']):
+            print(f"loop #{count}")
+            for olt in range(1, 2):
+                start_time = time.time()
+                (comm, command, quit_ssh) = ssh(olt_devices[str(olt)])
+                command("scroll 512")
+                clts = client_ports(comm, command, str(olt))
+                clients.extend(clts)
+                quit_ssh()
+                end_time = time.time()
+                ttl_time = end_time - start_time
+                print(
+                    f"the ttl amount of time for a given olt [max] is : {ttl_time} secs | {ttl_time/60} min"
+                )
+            db_request(endpoints["empty_alarms"], {})
+            db_request(endpoints["add_alarms"], {"alarms": clients})
+            send_mail(clients)
+            count += 1
 
 
 if __name__ == "__main__":
