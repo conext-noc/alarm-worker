bd297ada6adc7e0498d96ab3260703bd39f27db8
Ricardo Vera <ricardovera71@gmail.com>
2024-01-21 17:22:57 -0600

    test(snmp)

--- scripts/CA.py
+++ scripts/CA.py
@@ -92,7 +92,7 @@ def los_clients(device, port_list, community, target, context):
                         oid_7,
                     )
                 )
-                print(var_bind_table[0])
+                print(var_bind_table)
                 descr = f"{var_bind_table[0]}".split(" = ")[1]
                 ont = {
                     "frame": client["frame"],
