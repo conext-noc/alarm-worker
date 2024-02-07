da6c30fbaa3c1bd0f36e17b8ecb041680cc95774
Cesar Sanchez <cs96548@gmail.com>
2024-02-07 08:51:01 -0400

    test

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
     while True:
         if bool(
             datetime.now().strftime("%I:%M%p")
-            in ["07:30AM", "12:04PM","4:00PM"]
+            in ["07:30AM", "12:04PM","4:00PM","8:52AM"]
         ):
         # if True:
             print("\n")
@@ -33,7 +33,7 @@ def main():
             while not bool(
                 datetime.now().strftime("%I:%M%p")
                 in [
-                    "07:40AM","12:10PM","4:10PM"
+                    "07:40AM","12:10PM","4:10PM","8:56AM"
                 ]
             ):
                 log(
